62e6f35fe12ed30126da2d2cb1c6de3e35b28480
James Long <jlongster@jlongster.com>
2012-07-16 16:55:19 -0400

    scrub scripts

--- static/js/livecss.js
+++ static/js/livecss.js
@@ -89,6 +89,8 @@ function hideError() {
 function setHTML() {
     if(applying) {
         var html = window.htmlDoc.getText();
+        html = html.replace('<script', '');
+        html = html.replace('&lt;script', '');
         $('#canvas').html(html);
     }
 }
